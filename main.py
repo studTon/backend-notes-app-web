81cb6a26b295ba1c73dac70ca311c5fe243795c7
studTon <evertonsdasilva1@gmail.com>
2021-09-20 11:51:39 -0300

    Coloquei uma busca dentro da aplicação

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from typing import get_args
+from flask import Flask, request
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return '<h1>Anotações</h1>'
+    retorno = None
+
+    nota = request.args.get('Digite algo')
+
+    retorno = get_args(nota)
+
+    return render_template('index.html', nota=nota, retorno=retorno)
 
 if __name__ == "__main__":
     app.run(debug=True)
