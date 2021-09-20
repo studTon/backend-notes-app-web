071a2d2ba08c7950719d32d7d66ac9e38a48b5eb
studTon <evertonsdasilva1@gmail.com>
2021-09-20 10:16:57 -0300

    Reconstrução do código para app web

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'data':'hello world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route('/')
+def main():
+    return '<h1>Anotações</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
