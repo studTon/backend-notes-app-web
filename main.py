01fd6f17ffbfaae3c9bdb02cbcc63626d70e01a0
studTon <evertonsdasilva1@gmail.com>
2021-09-29 13:24:50 -0300

    Problema na criação de métodos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask.globals import request
 from data import json_object
 
@@ -8,17 +8,19 @@ app = Flask(__name__)
 def delete():
     return delete
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def index():
-    return jsonify({"About":"hello world"})
-    
+def hello():
+    return 'hello world'
+'''class nota():
+    def __init__(self) -> None:
+        pass;
+        self.title = request.form['title']
+        self.id = request.form['id']
+        self.date = request.form['date']
+        self.description = request.form['description']'''
 
-@app.route('/get/<text>', methods=['GET'])
-def criar_nota(text):
-    return jsonify({
-        "title":text,
-        "date": 2021,
-        "description": "Ok",
-        "id": 0})
+@app.route('/get', methods=['GET'])
+def criar_nota():
+    return render_template('template.html')
 @app.route('/post/<data>', methods=['POST'])
 def editar_nota(data):
     return jsonify({
@@ -27,5 +29,6 @@ def editar_nota(data):
         'description': 'Ok',
         'id': 0
     })
+
 if __name__ == "__main__":
     app.run(debug=True)
