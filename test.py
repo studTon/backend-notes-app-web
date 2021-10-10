52eb52e989b31a15f95e7df00c688e9fa1512d8f
studTon <evertonsdasilva1@gmail.com>
2021-10-10 11:39:13 -0300

    Exclui o test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-BASE = "http://127.0.0.1:5000/"
-
-
-response = requests.post(BASE + "note/0", {"title":"Comida", "description":"Lasanha", "date":"Mar 30, 2021"})
-response = requests.get(BASE + "note/0")
-print(response.json())
